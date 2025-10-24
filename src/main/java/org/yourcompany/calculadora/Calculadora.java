758c51fb16eddd4d6d6d18fd38267379b6932770
yunss-el <elhailoufiyounes342@gmail.com>
2025-10-24 12:33:34 +0200

    Afegit funció llancament monedes i funció preu entrada.

--- src/main/java/org/yourcompany/calculadora/Calculadora.java
+++ src/main/java/org/yourcompany/calculadora/Calculadora.java
@@ -1,5 +1,3 @@
-
-
 package org.yourcompany.calculadora;
 
 /**
@@ -7,60 +5,49 @@ package org.yourcompany.calculadora;
  * @author Posa el teu nom
  */
 
-
+import java.util.Random;
 public class Calculadora {
 
     public static void main(String[] args) {
-
-        // Exemples de crida per al mètode nombreDigits
-        System.out.println("Nombre de dígits de 1234: " + nombreDigits(1234));
-        System.out.println("Nombre de dígits de 56789: " + nombreDigits(56789));
-        System.out.println("Nombre de dígits de -101010: " + nombreDigits(-101010));
-        System.out.println("Nombre de dígits de 0: " + nombreDigits(0));
-
-        // Exemples de crida per al mètode sumaPrimersNumeros
-        // System.out.println("Suma dels primers 5 números: " + sumaPrimersNumeros(5));
-        // System.out.println("Suma dels primers 10 números: " + sumaPrimersNumeros(10));
-        // System.out.println("Suma dels primers 15 números: " + sumaPrimersNumeros(15));
-
-        // Exemples de crida per al mètode calcularFactorial
-        // System.out.println("Factorial de 5: " + calcularFactorial(5));
-        // System.out.println("Factorial de 7: " + calcularFactorial(7));
-        // System.out.println("Factorial de 10: " + calcularFactorial(10));
-
-        // Exemples de crida per al mètode sumaQuadrats
-        // System.out.println("Suma dels quadrats dels primers 3 números: " + sumaQuadrats(3));
-        // System.out.println("Suma dels quadrats dels primers 5 números: " + sumaQuadrats(5));
-        // System.out.println("Suma dels quadrats dels primers 7 números: " + sumaQuadrats(7));
-
-        // Exemples de crida per al mètode calcularPotencia
-        // System.out.println("2 elevat a la potència 3: " + calcularPotencia(2, 3));
-        // System.out.println("5 elevat a la potència 4: " + calcularPotencia(5, 4));
-        // System.out.println("3 elevat a la potència 5: " + calcularPotencia(3, 5));
-
-       
+        int[] result100 = nombreLLancaments(100);
+        int[] result1000 = nombreLLancaments(1000);
+        int[] result2000 = nombreLLancaments(2000);
         
+        System.out.println("100 llancaments - cares: " + result100[0] + ", creus: " + result100[1]);
+        System.out.println("1000 llancaments - cares: " + result1000[0] + ", creus: " + result1000[1]);
+        System.out.println("2000 llancaments - cares: " + result2000[0] + ", creus: " + result2000[1]);
+        preuEntrada(true, false, 5);
     }
 
-    /**
-     * Calcula el nombre de dígits d’un número enter.
-     * Funciona tant per a nombres positius com negatius.
-     * Si el nombre és 0, retorna 1.
-     *
-     * @param nombre Número del qual es vol saber el nombre de dígits
-     * @return Quantitat de dígits que té {@code nombre}
-     */
-    public static int nombreDigits(int nombre) {
-        if (nombre == 0) {
-            return 1; // el 0 té un dígit
-        }        
-        int comptador = 0;
-        while (nombre != 0) {
-            nombre /= 10;
-            comptador++;
+    public static int[] nombreLLancaments(int veces) {
+        Random random = new Random();
+        int caras = 0;
+        int creus = 0;
+
+        for (int i = 0; i < veces; i++) {
+            int lanzamiento = random.nextInt(2);
+            if (lanzamiento == 0) {
+                caras++;
+            } else {
+                creus++;
+            }
         }
-        return comptador;
+
+        return new int[]{caras, creus};
+    } 
+    public static void preuEntrada(boolean capDeSetmana, boolean carnetJove, double Preu) {
+        // TODO code application logic here
+        if (capDeSetmana == false){
+            Preu = 5;
+        } else if (capDeSetmana == true){
+            Preu = Preu+(Preu*10/100);
+        }
+        if (carnetJove == true){
+            Preu = Preu-(Preu*15/100);
+            System.out.println("El preu de l'entrada son: " + Preu + " euros");
+        } else if (carnetJove == false){
+            System.out.println("El preu de l'entrada son: " + Preu + " euros");
+        }
     }
-
-
+    
 }
